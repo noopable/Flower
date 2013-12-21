e5f4fc579a1f65931e7a05f59aa65c425d097d53
Tomoaki Kosugi <kipspro@gmail.com>
2013-12-21 10:42:51 +0900

    Add exceptions

---
+++ src/Flower/View/Pane/Exception/PaneClassNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ *
+ *
+ * @copyright Copyright (c) 2013-2013 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+namespace Flower\View\Pane\Exception;
+
+/**
+ * Description of PaneClassNotFoundeException
+ *
+ * @author tomoaki
+ */
+class PaneClassNotFoundException implements \Flower\Exception\RuntimeException{
+}
