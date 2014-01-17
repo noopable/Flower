9c4aaaae38b4a5849d65b4f1b052079f69257552
Tomoaki Kosugi <kipspro@gmail.com>
2014-01-17 23:00:25 +0900

    Add service it can get Repository

---
+++ src/Flower/ServiceLayer/AbstractRepoService.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * 
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\ServiceLayer;
+
+use Flower\Model\RepositoryPluginManagerAwareInterface;
+use Flower\Model\RepositoryPluginManagerAwareTrait;
+use Flower\Model\RepositoryInterface;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
+/**
+ * Description of AbstractRepoService
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class AbstractRepoService extends AbstractService 
+    implements RepositoryPluginManagerAwareInterface {
+    use RepositoryPluginManagerAwareTrait;
+    
+    protected $repository;
+    
+    protected $repositoryName;
+    
+    public function setRepositoryName($repositoryName)
+    {
+        $this->repositoryName = $repositoryName;
+    }
+    
+    public function getRepositoryName()
+    {
+        return $this->repositoryName;
+    }
+            
+    
+    public function setRepository(RepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+    
+    public function getRepository()
+    {
+        if (false === $this->repository) {
+            return;
+        }
+        if (!isset($this->repository) && isset($this->repositoryName)) {
+            try {
+                if ($this->getRepositoryPluginManager()->has($this->repositoryName)) {
+                    $this->repository = $this->getRepositoryPluginManager()->get($this->repositoryName);
+                } else {
+                    $this->repository = $this->getRepositoryPluginManager()->byName($this->repositoryName);
+                }
+            } catch (ServiceNotFoundException $ex) {
+                //腐ってやがる。早すぎたんだ。 or 設定ミス？
+                $this->repository = false;
+                throw new Exception\RuntimeException('repository name '. $this->repositoryName . ' is not found?', 0, $ex);
+            }
+        }
+        return $this->repository;
+    }
+}
