dd048cae85495b2e0b9cfc22aa496064e320177d
Tomoaki Kosugi <kipspro@gmail.com>
2014-01-24 16:59:25 +0900

    Add DomainException

---
+++ src/Flower/Exception/DomainException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\Exception;
+
+/**
+ * Description of DomainException
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class DomainException extends \DomainException implements ExceptionInterface{
+}
