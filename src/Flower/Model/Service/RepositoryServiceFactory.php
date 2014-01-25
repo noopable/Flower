47b421da3f9778ff26a2e1d03ffdec0f4eba7580
Tomoaki Kosugi <kipspro@gmail.com>
2014-01-25 21:24:31 +0900

    Add DiServiceInitializer to RepositoriesFactory

--- src/Flower/Model/Service/RepositoryServiceFactory.php
+++ src/Flower/Model/Service/RepositoryServiceFactory.php
@@ -1,11 +1,16 @@
 <?php
-namespace Flower\Model\Service;
+
 /*
  *
  *
  * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
  * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
  */
+namespace Flower\Model\Service;
+
+use Flower\Model\Exception\RuntimeException;
+use Zend\ServiceManager\Di\DiAbstractServiceFactory;
+use Zend\ServiceManager\Di\DiServiceInitializer;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
@@ -14,26 +19,19 @@ use Zend\ServiceManager\ServiceLocatorInterface;
  *
  */
 class RepositoryServiceFactory implements FactoryInterface {
-    
+
     /**
      *
      * @var string
      */
     protected $configId = 'flower_repositories';
-    
+
     /**
      *
      * @var string
      */
     protected $managerClass = 'Flower\Model\Service\RepositoryPluginManager';
-    
-    /**
-     * whether or not use DependencyInjector
-     * 
-     * @var bool
-     */
-    protected $useDi = true;
-    
+
     /**
      * @param  ServiceLocatorInterface $serviceLocator
      */
@@ -43,12 +41,14 @@ class RepositoryServiceFactory implements FactoryInterface {
             $class = $this->managerClass;
         }
         else {
-            trigger_error('class not found:' . $this->managerClass, E_USER_WARNING);
-            $class = 'Flower\Model\Service\RepositoryPluginManager';
+            throw new RuntimeException('repository plugin manager class not found:' . $this->managerClass);
         }
-        
-        $config = $serviceLocator->get('Config');
-        
+
+        if ($serviceLocator->has('Config')) {
+            $config = $serviceLocator->get('Config');
+        } else {
+            $config = array();
+        }
         
         if (isset($config[$this->configId])) {
             $managerConfig = $config[$this->configId];
@@ -57,11 +57,21 @@ class RepositoryServiceFactory implements FactoryInterface {
         else {
             $managerConfig = array('service_locator' => $serviceLocator);
         }
-        
+
         $service = new $class(new RepositoryPluginConfig($managerConfig));
-        
+
         $service->addPeeringServiceManager($serviceLocator);
-        
+
+        if ($serviceLocator->has('Di')) {
+            $di = $serviceLocator->get('Di');
+            $service->addAbstractFactory(
+                new DiAbstractServiceFactory($di, DiAbstractServiceFactory::USE_SL_NONE)
+            );
+            $service->addInitializer(
+                new DiServiceInitializer($di, $serviceLocator)
+            );
+        }
+
         return $service;
     }
 }
