5744b53947bebb697869211dc1825d43fbfbf0c5
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-12 21:03:00 +0900

    Add DomainName based integrationtest

---
+++ tests/FlowerTest/View/Pane/Service/TestAsset/test_domain_pane_cache_options.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+return array(
+    'listener_class' => 'Flower\View\Pane\ManagerListener\Domain\PaneCacheListener',
+    'domain_service' => 'Flower_Domain_Service',
+    //@see http://framework.zend.com/manual/2.2/en/modules/zend.cache.storage.adapter.html
+    'cache_storage' => array(
+        'adapter' => array(
+            'name'    => 'filesystem',
+            'options' => array(
+                'namespace' => 'test_pane_manager',
+                'cache_dir' => __DIR__ . '/tmp/cache',
+                'dir_level' => 1,
+            ),
+        ),
+        'plugins' => array(
+            'exception_handler' => array('throw_exceptions' => true),
+        ),
+    ),
+);
