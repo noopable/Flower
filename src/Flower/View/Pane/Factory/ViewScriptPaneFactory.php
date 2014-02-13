58aecef4f51d68777c7518dce391b163101624eb
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-14 08:11:35 +0900

    Add ViewScriptPane enables to render view script named in the var.

---
+++ src/Flower/View/Pane/Factory/ViewScriptPaneFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\View\Pane\Factory;
+
+/**
+ * Description of ViewScriptPaneFactory
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class ViewScriptPaneFactory extends ListPaneFactory
+{
+    protected static $paneClass = 'Flower\View\Pane\PaneClass\ViewScriptPane';
+
+}
---
+++ src/Flower/View/Pane/PaneClass/ViewScriptPane.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\View\Pane\PaneClass;
+
+use Flower\View\Pane\Exception\RuntimeException;
+use Flower\View\Pane\PaneRenderer;
+use Zend\View\Exception\RuntimeException as ZendViewException;
+
+/**
+ * Description of ViewScriptPane
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class ViewScriptPane extends ListPane
+{
+    protected static $factoryClass = 'Flower\View\Pane\Factory\ViewScriptPaneFactory';
+
+    public $containerTag = 'div';
+
+    public $wrapTag = 'div';
+
+    public $tag = 'div';
+
+    public function _render(PaneRenderer $paneRenderer)
+    {
+        $this->indent = $paneRenderer->indent;
+        $this->linefeed = $paneRenderer->linefeed;
+        $this->commentEnable = $paneRenderer->commentEnable;
+
+        $depth = $paneRenderer->getDepth();
+        $indent = str_repeat($this->indent, $depth + 1);
+        $response = '';
+
+        $response .= $indent . $this->begin($depth) . $this->linefeed;
+
+        $var = $this->_var;
+        if (is_string($var)) {
+            $view = $paneRenderer->getView();
+            if (!$view) {
+                throw new RuntimeException('paneRenderer has no PhpRenderer');
+            }
+            $var_comment = htmlspecialchars($var);
+            $response .= $this->commentEnable ? $indent . "<!-- start content $var_comment -->" . $this->linefeed : "";
+            try {
+                $response .= $view->render($var);
+            } catch (ZendViewException $ex) {
+                $response .= "<!-- error: " . $view->escapeHtml($ex->getMessage()) . " -->";
+            }
+            $response .= $this->commentEnable ? $indent . "<!-- end content $var_comment -->" . $this->linefeed : "";
+        }
+
+        $response .= $indent . $this->end($depth);
+
+        return $response;
+    }
+}
