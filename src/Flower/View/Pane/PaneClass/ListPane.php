4274bf1eaf47703de9ae5866c4fc82037b8c7bc9
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-14 09:03:32 +0900

    extract ListPane::ContainerBegin to Trait

--- src/Flower/View/Pane/PaneClass/ListPane.php
+++ src/Flower/View/Pane/PaneClass/ListPane.php
@@ -17,6 +17,8 @@ use Flower\View\Pane\PaneRenderer;
  */
 class ListPane extends Pane implements CallbackRenderInterface
 {
+    use ListContainerBeginEndTrait;
+
     protected static $factoryClass = 'Flower\View\Pane\Factory\ListPaneFactory';
 
     public $containerTag = 'ul';
@@ -38,64 +40,6 @@ class ListPane extends Pane implements CallbackRenderInterface
     public $containerEnd;
 
     protected $containerEndStack = array();
-
-    public function containerBegin($depth = null)
-    {
-        $renderSelf = false;
-        $response = '';
-        $containerEnd = '';
-        $indent = str_repeat($this->indent, (int) $depth);
-        $renderer = $this->getPaneRenderer();
-        $maxDepth = $renderer->getMaxDepth();
-
-        if ($depth === 0) {
-            //第１階層はulでラップする。
-            $response = $indent . $this->containerBegin;
-            $containerEnd .= $indent . $this->containerEnd;
-        }
-
-        if ($this->hasContent()) {
-            $renderSelf = true;
-            //自要素を表示する
-            if (strlen($response)) {
-                $response .= $this->linefeed . $indent;
-            }
-            $response .= $this->wrapBegin . $this->linefeed . //<li>
-                $this->_render($renderer) . $this->linefeed; //content
-        }
-        if ($this->valid() && (($maxDepth === false) || ($depth < $maxDepth))) {
-            //子要素をcontainerでラップする
-            $response .= $indent . $this->containerBegin; //<ul>
-            if ($renderSelf) {
-                //自要素のラップを後で閉じる
-                if (empty($containerEnd)) {
-                    $containerEnd =
-                        $this->containerEnd . $this->linefeed . //</ul>
-                        $indent . $this->wrapEnd;
-                } else {
-                    $containerEnd =
-                        $this->containerEnd . $this->linefeed . //</ul>
-                        $indent . $this->wrapEnd . $this->linefeed .//</li>
-                        $indent . $containerEnd;//</ul>
-                }
-            }
-        } elseif ($renderSelf) {
-            //自要素のラップをすぐに閉じる
-            $response .=  $indent . $this->wrapEnd; //</li>
-        } else {
-            //子要素も自要素も表示しないなら何も表示しない
-            $this->containerEndStack[] = '';
-            return '';
-        }
-
-        $this->containerEndStack[] = $containerEnd;
-        return $response;
-    }
-
-    public function containerEnd($depth = null)
-    {
-        return array_pop($this->containerEndStack);
-    }
 
     public function wrapBegin($depth = null)
     {
