77b7384490afc0935451dcd7fe5de558ec391504
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-14 11:50:53 +0900

    Add EntityScriptPaneFactory

---
+++ src/Flower/View/Pane/Factory/EntityScriptPaneFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\View\Pane\Factory;
+
+/**
+ * Description of EntityScriptPaneFactory
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class EntityScriptPaneFactory extends ListPaneFactory
+{
+    protected static $paneClass = 'Flower\View\Pane\PaneClass\EntityScriptPane';
+}
