797ac6c69bab64b432401c8052465127c03d9708
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-15 17:01:18 +0900

    extract CallbackRenderTrait from ListPane

--- src/Flower/View/Pane/PaneClass/ListPane.php
+++ src/Flower/View/Pane/PaneClass/ListPane.php
@@ -17,17 +17,9 @@ use Flower\View\Pane\PaneRenderer;
  */
 class ListPane extends Pane implements CallbackRenderInterface
 {
-    use ListContainerBeginEndTrait;
+    use CallbackRenderTrait, ListContainerBeginEndTrait;
 
     protected static $factoryClass = 'Flower\View\Pane\Factory\ListPaneFactory';
-
-    /**
-     *  var をcallbackとして使うため
-     *　render時に、実質的なvarとして働く
-     *
-     * @var type
-     */
-    public $_var;
 
     protected $containerEndStack = array();
 
@@ -39,61 +31,4 @@ class ListPane extends Pane implements CallbackRenderInterface
         $this->tag = 'span';
     }
 
-    public function render(PaneRenderer $paneRenderer)
-    {
-        $this->indent = $paneRenderer->indent;
-        $this->linefeed = $paneRenderer->linefeed;
-        $this->commentEnable = $paneRenderer->commentEnable;
-
-        $depth = $paneRenderer->getDepth();
-        $indent = str_repeat($this->indent, $depth + 1);
-
-        $response = '';
-        $response .= $this->commentEnable ? $indent . "<!-- start content CallbackRender -->" . $this->linefeed : '';
-        $response .= $indent . $this->wrapBegin($depth) . $this->linefeed;
-        $response .= $this->_render($paneRenderer) . $this->linefeed;
-        $response .= $indent . $this->wrapEnd($depth) . $this->linefeed;
-        $response .= $this->commentEnable ? $indent . "<!-- end content CallbackRender -->" . $this->linefeed : '';
-        return $response;
-    }
-
-    public function _render(PaneRenderer $paneRenderer)
-    {
-        $this->indent = $paneRenderer->indent;
-        $this->linefeed = $paneRenderer->linefeed;
-        $this->commentEnable = $paneRenderer->commentEnable;
-
-        $depth = $paneRenderer->getDepth();
-        $indent = str_repeat($this->indent, $depth + 1);
-        $innerIndent = $indent . $this->indent;
-        $response = '';
-
-        $response .= $indent . $this->begin($depth) . $this->linefeed;
-
-        $var = $this->_var;
-        if (is_string($var)) {
-            $var_comment = htmlspecialchars($var);
-            $response .= $this->commentEnable ? $indent . "<!-- start content $var_comment -->" . $this->linefeed : "";
-            if (isset($paneRenderer->getVars()->$var)) {
-                $response .= $paneRenderer->getVars()->$var . $this->linefeed;
-            } else {
-                $response .= $this->commentEnable ? $innerIndent . "<!-- var $var_comment is not found -->" . $this->linefeed : "";
-            }
-            $response .= $this->commentEnable ? $indent . "<!-- end content $var_comment -->" . $this->linefeed : "";
-        } elseif ($var instanceof Closure) {
-            $response .= $this->commentEnable ? $indent . "<!-- start content Closure -->" . $this->linefeed : "";
-            $response .= $var($paneRenderer) . $this->linefeed;
-            $response .= $this->commentEnable ? $indent . "<!-- end content Closure -->" . $this->linefeed : "";
-        }
-
-        $response .= $indent . $this->end($depth);
-
-        return $response;
-    }
-
-    public function hasContent()
-    {
-        //empty() is true when '0'
-        return !empty($this->_var) || '0' === $this->_var;
-    }
 }
