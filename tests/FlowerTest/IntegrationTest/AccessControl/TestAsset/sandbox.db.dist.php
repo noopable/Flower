dcb947998d66896a5a095c14ae7bf0c52aa336d0
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-19 21:32:10 +0900

    Add test dirs

---
+++ tests/FlowerTest/IntegrationTest/AccessControl/TestAsset/sandbox.db.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+
+return array(
+    'driver'         => 'Pdo',
+    'pdodriver'      => 'mysql',
+    'username' => '',
+    'password' => '',
+    'database' => 'sandbox',
+    'characterset' => 'UTF8',
+    'driver_options' => array(
+        //PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+    ),
+    'host' => 'localhost',
+);
