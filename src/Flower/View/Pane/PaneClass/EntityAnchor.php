0e2f11d9612f618c9d9f922e5011934fb60ae61c
Tomoaki Kosugi <kipspro@gmail.com>
2014-02-19 12:51:45 +0900

    Add EntityAnchor implements EntityAwareInterface

---
+++ src/Flower/View/Pane/PaneClass/EntityAnchor.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\View\Pane\PaneClass;
+
+/**
+ * Description of EntityAnchor
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class EntityAnchor extends Anchor implements EntityAwareInterface
+{
+    use MutableEntityAwareTrait;
+
+}
---
+++ src/Flower/View/Pane/PaneClass/MutableEntityAwareTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\View\Pane\PaneClass;
+
+use ArrayAccess;
+
+/**
+ * Description of MutableEntityAwareTrait
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+trait MutableEntityAwareTrait
+{
+    protected $entity;
+
+    public function setEntity($entity)
+    {
+        $mutableParams = $this->getOption('mutable_params');
+        if (!empty($mutableParams)) {
+            foreach ($mutableParams as $key) {
+                if (is_object($entity)) {
+                    $method = 'get' . lcfirst($key);
+                    if (method_exists($entity, $method)) {
+                        $this->$key = $entity->$method();
+                    } elseif(isset($entity->$key) && !empty($entity->$key)) {
+                        $this->$key = $entity->$key;
+                    } elseif($entity instanceof ArrayAccess && isset($entity[$key])) {
+                        $this->$key = $entity[$key];
+                    }
+                } elseif (is_array($entity) && isset($entity[$key])) {
+                    $this->$key = $entity[$key];
+                }
+            }
+        }
+
+        $this->entity = $entity;
+    }
+
+    public function getEntity()
+    {
+        return $this->entity;
+    }
+}
