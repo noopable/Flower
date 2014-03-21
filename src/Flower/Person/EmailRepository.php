2a2fbe5e42f0d6e1698fa6da3998d97a49f60deb
Tomoaki Kosugi <kipspro@gmail.com>
2014-03-21 20:21:47 +0900

    Add EmailRepository Update PersonRepository

---
+++ src/Flower/Person/EmailRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace Flower\Person;
+
+use Flower\Model\AbstractDbTableRepository;
+
+/**
+ * Description of EmailRepository
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class EmailRepository extends AbstractDbTableRepository
+{
+
+}
--- src/Flower/Person/PersonRepository.php
+++ src/Flower/Person/PersonRepository.php
@@ -9,10 +9,59 @@
 namespace Flower\Person;
 
 use Flower\Model\AbstractDbTableRepository;
+use Flower\Person\Identical\EmailInterface;
+
 /**
  * Description of PersonRepository
  *
  * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
  */
 class PersonRepository extends AbstractDbTableRepository {
+
+    protected $emailRepository;
+
+    public function getPerson($personId)
+    {
+        $person = $this->getEntity(array('person_id' => $personId));
+        $emails = $this->getEmailRepository()->getCollection(array('primary_person_id' => $personId));
+        foreach ($emails as $email) {
+            if ($email instanceof EmailInterface) {
+                $person->addEmail($email);
+            }
+        }
+        return $person;
+    }
+
+    public function savePerson(PersonInterface $person)
+    {
+        $emails = $person->getEmails();
+        $emailRepository = $this->getEmailRepository();
+        //start transaction start?
+        //アダプターからコネクションを取得して開始する。
+        try {
+            $this->beginTransaction();
+            foreach ($emails as $email) {
+                if ($email instanceof EmailInterface) {
+                    $emailRepository->save($email);
+                }
+            }
+            $res = $this->save($person);
+            $this->commit();
+        } catch (\Exception $ex) {
+            $this->rollback();
+            throw $ex;
+        }
+
+        return $res;
+    }
+
+    public function setEmailRepository(EmailRepository $emailRepository)
+    {
+        $this->emailRepository = $emailRepository;
+    }
+
+    public function getEmailRepository()
+    {
+        return $this->emailRepository;
+    }
 }
