29b3c15cf1d34657bb2e3ef5e14a0919b082e733
Tomoaki Kosugi <kipspro@gmail.com>
2014-05-14 21:53:22 +0900

    sort out ResourceInterface

--- src/Flower/Resource/ResourceClass/ResourceInterface.php
+++ src/Flower/Resource/ResourceClass/ResourceInterface.php
@@ -7,12 +7,12 @@
  */
 namespace Flower\Resource\ResourceClass;
 /*
- * Resource feeds raw materials for document. 
- *  
+ * Resource feeds raw materials for document.
+ *
  * Zend\Acl\Resourceとは違う、Documentの情報源としてのリソース
  * ファイルまたはRedisのようなKVSに保存する。
  * Zend\Cacheを利用してもよい。
- * 
+ *
  */
 
 /**
@@ -24,30 +24,30 @@ interface ResourceInterface {
     /**
      * グローバルで一意となるID　
      *  ($type_$innerIdを推奨)
-     * 
+     *
      */
     public function getResourceId();
-    
+
     public function setResourceId($resourceId);
     /**
      * $typeスコープ内で一意となるID　
      * （10進数値表現文字列を推奨）
-     * 
+     *
      */
     public function getInnerId();
     public function setInnerId($innerId);
-    
+
     public function setType($type);
     public function getType();
-    
+
     public function setOptions(array $options);
     public function getOptions();
-    
+
     public function setProperties(array $properties);
     public function getProperties();
-    
+
     public function setData($data, $serialized = false);
     public function getData($serialize = false);
-    
+
     public function toString();
 }
