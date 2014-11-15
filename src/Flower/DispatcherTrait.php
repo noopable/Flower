35aa1eeeac94cfd1940bc07d9f8b0752f7697bd1
Tomoaki Kosugi <kipspro@gmail.com>
2014-11-16 05:10:29 +0900

    Update DispatcherTrait

--- src/Flower/DispatcherTrait.php
+++ src/Flower/DispatcherTrait.php
@@ -1,57 +1,57 @@
 <?php
-namespace Flower;
 /*
  *
  *
  * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
  * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
  */
-use Zend\View\Model\ViewModel;
+
+namespace Flower;
 
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
 
 /**
- * Description of ViewModelFromDispatch
+ * Description of DispatcherTrait
  *
  * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
  */
 trait DispatcherTrait {
     use ServiceLocatorAwareTrait;
-    
-    protected $controller;
-    
+
+    protected $controllerName;
+
     protected $dispatchOptions;
-    
+
     protected $signature;
-    
+
     public function setControllerName($controllerName)
     {
         $this->controllerName = $controllerName;
     }
-    
+
     public function setDispatchOptions($options)
     {
         $this->dispatchOptions = $options;
     }
-    
+
     public function setSignature($signature)
     {
         $this->signature = $signature;
     }
-    
-    protected function dispatch()
+
+    public function dispatch()
     {
         if (!isset($this->serviceLocator)) {
             throw new Exception\RuntimeException('ServiceLocator is required in the Dispatch Process');
         }
-        
+
         if (!isset($this->controllerName)) {
             throw new Exception\RuntimeException('ControllerName is required in the Dispatch Process');
         }
-        
+
         $pluginManager = $this->serviceLocator->get('ControllerPluginManager');
         $forwarder = $pluginManager->get('forward');
         return $forwarder->dispatch($this->controllerName, $this->dispatchOptions);
     }
-    
+
 }
---
+++ tests/FlowerTest/TestAsset/DispatcherTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2013-2014 KipsProduction (http://www.kips.gr.jp)
+ * @license   http://www.kips.gr.jp/newbsd/LICENSE.txt New BSD License
+ */
+
+namespace FlowerTest\TestAsset;
+
+use Flower\DispatcherTrait as FlowerDispatcherTrait;
+
+/**
+ * Description of DispatcherTrait
+ *
+ * @author Tomoaki Kosugi <kosugi at kips.gr.jp>
+ */
+class DispatcherTrait
+{
+    use FlowerDispatcherTrait;
+}
